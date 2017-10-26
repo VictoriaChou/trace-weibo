0423e2079311b96aee7e111e33778b338799fb25
VictoriaChou <zhoufuxing@sjtu.edu.cn>
2017-10-26 21:25:53 +0800

    modify

--- k-means.py
+++ k-means.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# tf-idf建模，k-means聚类
+# Author: Zhou Fuxing
+
 import numpy as np
 import pandas as pd
 import re
@@ -7,6 +11,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
 from sklearn.externals import joblib
 from db2text import get_tweets
+from clean_txt import clean_txt
 
 f1 = open('stopwords.txt','r')
 stopwords = [l.strip() for l in f1.readlines()]
@@ -15,15 +20,16 @@ tfidf_vectorizer = TfidfVectorizer(max_df = 0.8, stop_words = stopwords,
                                 min_df = 10, use_idf = True, ngram_range = (1, 3))
 
 tweets = get_tweets()
-tfidf_matrix = tfidf_vectorizer.fit_transform(tweets)
+clean_tweets = []
+for tweet in tweets:
+    clean_txt(tweet)
+    clean_tweets.append(tweet)
+tfidf_matrix = tfidf_vectorizer.fit_transform(clean_tweets)
 print(tfidf_matrix.shape)
 
 # 获取tf-idf矩阵中的特征表
 terms = tfidf_vectorizer.get_feature_names()
-dist = 1 - cosine_similarity(tfidf_matrix)
-
-
-num_clusters = 20
+num_clusters = 10
 
 km = KMeans(n_clusters = num_clusters)
 km.fit(tfidf_matrix)
